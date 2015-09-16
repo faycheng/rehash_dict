d7731f22873a005a6fca6ddf44e2a5c56a787d07
chengxuyuanfei <chengxuyuanfei@gmail.com>
2015-09-16 16:08:11 +0800

    修改dictEntry中value未union，add_dict改为可变参数传值，处理多类型值结构，使用**替代*指向dictEntry表，更加直观，容易释放内存，和节省内存

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main(void)
       char *s3 = "strthree";
     d = create_dic();
     add_dict(d, ss, 0, 1024);
-    add_dict(d, s2, 1, 1024.1024);
+    add_dict(d, s2, 1, 1024.124);
     add_dict(d, s3, 2, "hello dict");
     return 0;
 }
